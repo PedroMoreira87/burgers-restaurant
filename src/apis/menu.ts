34589d53be6782dc541f1ffa5a46858bde9dd28c
PedroHenriqueMoreira <pedrohmoreira87@gmail.com>
2024-12-03 18:04:35 -0500

    feat(menu): add menu

--- src/apis/menu.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const getMenu = async () => {
-  const response = await axios.get('https://cdn-dev.preoday.com/challenge/menu');
-  return response.data;
-};
--- src/apis/restaurant.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const getRestaurant = async () => {
-  const response = await axios.get('https://cdn-dev.preoday.com/challenge/venue/9');
-  return response.data;
-};
