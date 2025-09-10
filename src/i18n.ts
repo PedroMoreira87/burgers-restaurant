264d3dad9e0a3f89de23261db1692b2e6c6377a0
Pedro Henrique Moreira <pedrohmoreira87@gmail.com>
2025-09-10 15:56:52 -0300

    fix(comment): remove comment

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,7 +3,7 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
 import en from './locales/en/translation.json';
-import pt from './locales/pt/translation.json'; // Example for Spanish
+import pt from './locales/pt/translation.json';
 
 i18n
   .use(LanguageDetector)
